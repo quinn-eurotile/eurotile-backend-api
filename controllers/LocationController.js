01ab4db03af053f678be93b6dbd22fad4987bc3f
Gurjeet <info@netqom.com>
2025-04-30 15:20:22 +0530

    Country State City Api

--- controllers/LocationController.js
+++ controllers/LocationController.js
@@ -6,28 +6,34 @@ const City = require('../models/City');
 
 module.exports = class LocationController {
 
+    /** Get List Of Countries */
     async getCountries(req, res) {
         try {
             const countries = await Country.find({}, { _id: 1, name: 1 });
-            return res.status(201).json({ message: "", data: countries, });
+            const formatted = countries.map(country => ({ label: country.name, value: country._id }));
+            return res.status(201).json({ message: "", data: formatted, });
         } catch (error) {
             return res.status(error?.statusCode || 500).json({ message: error?.message });
         }
     }
 
+    /** Get List Of States Based On Selected Country Id */
     async getStatesByCountryId(req, res) {
         try {
             const states = await State.find({ country_id: parseInt(req.params.country_id) }, { _id: 1, name: 1 });
-            return res.status(201).json({ message: "", data: states, });
+            const formatted = states.map(country => ({ label: country.name, value: country._id }));
+            return res.status(201).json({ message: "", data: formatted, });
         } catch (error) {
             return res.status(error?.statusCode || 500).json({ message: error?.message });
         }
     }
 
+     /** Get List Of Cities Based On Selected State Id */
     async getCitiesByStateId(req, res) {
         try {
-            const cities = await City.find({ state_id: parseInt(req.params.state_id) }, { _id: 1, name: 1});
-            return res.status(201).json({ message: "", data: cities, });
+            const cities = await City.find({ state_id: parseInt(req.params.state_id) }, { _id: 1, name: 1 });
+            const formatted = cities.map(country => ({ label: country.name, value: country._id }));
+            return res.status(201).json({ message: "", data: formatted, });
         } catch (error) {
             return res.status(error?.statusCode || 500).json({ message: error?.message });
         }
