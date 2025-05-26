024f0c8b2905eff43db3e1f58b8d94f15151258e
Gurjeet <info@netqom.com>
2025-05-26 10:46:13 +0530

    Added path for file in message collection

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const app = require("./app");
-
-
-const http = require("http");
 const port = process.env.PORT;
 const routes = require('./routes/index');
-console.log('routes comming')
+console.log('routes comming');
 const models = require('./models/index');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const server = http.createServer(app);
+
+const io = socketIo(server, {
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST']
+	}
+});
+
+io.on('connection', socket => {
+	console.log('Socket connected:', socket.id);
+
+	socket.on('sendMessage', msg => {
+		io.emit('receiveMessage', msg);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Socket disconnected:', socket.id);
+	});
+});
 
 // server listening 
 server.listen(port, () => {
