038f09e78fec87fe726c9d14102a7a9ffff5ae02
netqom <info@netqom.com>
2025-05-28 15:54:33 +0530

    Add file api

--- routes/CommonRoutes.js
+++ routes/CommonRoutes.js
@@ -6,7 +6,7 @@ const upload = multer(); // If you haven't set a custom storage yet
 const commonController = new CommonController();
 /* Order Management */
 router.get('/profile', multer().any(), auth, commonController.getProfile);
-router.put('/profile', multer().any(), auth, commonController.updateProfile);
-router.put("/profile/password",  multer().any(), auth, commonController.updateUserPassword);
+router.put('/profile/:id', multer().any(), auth, commonController.updateProfile);
+router.put("/profile/password/:id",  multer().any(), auth, commonController.updateUserPassword);
 
 module.exports = router;
