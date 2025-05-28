c2624859e7318b60714b25e97ef973db76e84906
NQD-013\gurjeet <info@netqom.com>
2025-05-28 17:06:14 +0530

    Add: staus

--- services/orderService.js
+++ services/orderService.js
@@ -77,6 +77,18 @@ class Order {
                             }
                         },
 
+                        {
+                            $lookup: {
+                                from: "users",            // collection to join
+                                localField: "createdBy",  // field in orders
+                                foreignField: "_id",      // field in users
+                                as: "createdByDetails"    // output array field
+                            }
+                        },
+                        {
+                            $unwind: "$createdByDetails" // optional: convert array to object if only one user per order
+                        },
+
                         // Select required fields
                         {
                             $project: {
@@ -87,6 +99,14 @@ class Order {
                                 totalAmount: 1,
                                 createdAt: 1,
                                 updatedAt: 1,
+                                createdBy: 1,
+                                updatedBy: 1,
+                                createdByDetails:{
+                                    _id : 1,
+                                    name : 1,
+                                    email : 1,
+                                    userImage : 1
+                                },
                                 orderDetails: {
                                     productId: 1,
                                     productDetail: 1,
