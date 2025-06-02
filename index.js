ed1c627217f6f66de22645d20e3cd330e137b94a
Gurjeet <info@netqom.com>
2025-06-02 15:11:30 +0530

    socket changes

--- index.js
+++ index.js
@@ -4,15 +4,42 @@ const routes = require('./routes/index');
 console.log('routes comming');
 const models = require('./models/index');
 const http = require('http');
+const https = require('https');
 const socketIo = require('socket.io');
 const { SupportTicketMsg } = require('./models');
 const { default: mongoose } = require("mongoose");
 
-const server = http.createServer(app);
+const SSL_CERT_PATH = process.env.SSL_CERT_PATH || '';
+const SSL_KEY_PATH = process.env.SSL_KEY_PATH || '';
+
+let server;
+
+if (process.env.APP_URL?.startsWith('https://')) {
+	try {
+		if (!SSL_CERT_PATH || !SSL_KEY_PATH) {
+			throw new Error("SSL paths not provided.");
+		}
+
+		const httpsOptions = {
+			cert: fs.readFileSync(SSL_CERT_PATH),
+			key: fs.readFileSync(SSL_KEY_PATH),
+		};
+
+		server = https.createServer(httpsOptions, app);
+		console.log("HTTPS server created");
+	} catch (error) {
+		console.error("Error creating HTTPS server:", error.message);
+		console.log("Falling back to HTTP server");
+		server = http.createServer(app);
+	}
+} else {
+	server = http.createServer(app);
+	console.log("HTTP server created");
+}
 
 const io = socketIo(server, {
 	cors: {
-		origin: 'http://localhost:3000',
+		origin: process.env.CLIENT_URL,
 		methods: ['GET', 'POST']
 	}
 });
